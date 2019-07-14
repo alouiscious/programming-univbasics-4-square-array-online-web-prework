3b2fca7775e7d3d4057c510acdb7bc16ad506cb0
Alouiscious Sims <info@reneesoul.com>
2019-07-14 03:40:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 def square_array(array)
   # your code here
-  count = 0
-  numbers = [1, 2, 3]
-
-  while count < numbers.length
-    numbers[count] = numbers[count] * numbers[count]
-    count += 1
-  end
-return numbers
+#   count = 0
+#   numbers = [1, 2, 3]
+#
+#   while count < numbers.length
+#     numbers[count] = numbers[count] * numbers[count]
+#     count += 1
+#   end
+# return numbers
 
   new_numbers = [9, 10, 16, 25]
   count = 0
